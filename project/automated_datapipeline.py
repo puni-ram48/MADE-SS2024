39ea4aca7901a55036e4ac7be31194a3595bda55
Puneetha Dharmapura Shrirama <dspuneetha@gmail.com>
2024-06-06 11:11:11 +0200

    Update automated_datapipeline.py
    updated pipeline with some minor changes

--- project/automated_datapipeline.py
+++ project/automated_datapipeline.py
@@ -2,40 +2,37 @@ import pandas as pd
 import numpy as np
 import requests
 import zipfile
-import os
+import io
 from sqlalchemy import create_engine
+import os
 
-def download_and_extract(url, extract_to):
+def download_and_extract_in_memory(url):
     """
-    Download a ZIP files from a given URL and extract its content to a specified directory.
-    
-    Args:
-        url (str): URL of the ZIP file to download.
-        extract_to (str): Directory where the contents of the ZIP file will be extracted.
+    Downloads a ZIP file from the specified URL and extracts its contents into memory.
+
+    Parameters:
+    url (str): The URL of the ZIP file to be downloaded.
 
     Returns:
-        None
-    """
-    local_zip_path = os.path.join(extract_to, os.path.basename(url))
+    dict: A dictionary where the keys are the names of the files within the ZIP archive, 
+          and the values are in-memory file-like objects containing the file contents."""
+
     response = requests.get(url)
-    with open(local_zip_path, 'wb') as file:
-        file.write(response.content)
-    with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
-    os.remove(local_zip_path)
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+        return {name: io.BytesIO(zip_ref.read(name)) for name in zip_ref.namelist()}
 
-def load_and_clean_data(file_path, delimiter=','):
+def load_and_clean_data(file_like, delimiter=','):
     """
-    Load a CSV file into a pandas DataFrame and drop rows with missing values.
-    
-    Args:
-        file_path (str): Path to the CSV file to read the data.
-        delimiter (str): Delimiter used in the CSV file. Default is ','.
-        
+    Load data from a CSV file-like object into a pandas DataFrame and perform data cleaning.
+
+    Parameters:
+    file_like (file-like object): The file-like object containing the CSV data.
+    delimiter (str, optional): The delimiter used in the CSV file. Default is ','.
+
     Returns:
-        pd.DataFrame: Cleaned DataFrame with no missing values.
+    pd.DataFrame: A cleaned DataFrame with no missing values.
     """
-    df = pd.read_csv(file_path, delimiter=delimiter)
+    df = pd.read_csv(file_like, delimiter=delimiter)
     df.dropna(inplace=True)
     return df
 
@@ -81,21 +78,6 @@ def drop_rows_with_zeros(df, columns):
     df = df[(df[columns] != 0).all(axis=1)]
     return df
 
-def fetch_data_from_db(db_path, table_name):
-    """
-    Fetch data from a specified table in a SQLite database and load it into a pandas DataFrame.
-    
-    Args:
-        db_path (str): Path to the SQLite database file.
-        table_name (str): Name of the table in the SQL database where the data will be written.
-        
-    Returns:
-        pd.DataFrame: DataFrame containing the data from the specified table.
-    """ 
-    engine = create_engine(f'sqlite:///{db_path}')
-    df = pd.read_sql_table(table_name, engine)
-    return df
-
 def create_quarterly_column(df):
     """
     Creates a new column indicating the corresponding quarter for each month in a DataFrame.
@@ -112,11 +94,26 @@ def create_quarterly_column(df):
         7: 'Q3', 8: 'Q3', 9: 'Q3',
         10: 'Q4', 11: 'Q4', 12: 'Q4'
     }
-    # Add a new column for quarters
     df['Quarter'] = df['Month'].map(month_to_quarter)
     return df
 
-def transform_wildfire_data(file_path):
+def create_database(db_dir, db_name):
+    """
+    Create a SQLite database in the specified directory.
+
+    Parameters:
+    db_dir (str): Directory path where the database will be created.
+    db_name (str): Name of the SQLite database.
+
+    Returns:
+    str: Path of the created SQLite database.
+    """
+    db_path = os.path.join(db_dir, f"{db_name}.db")
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+    return db_path
+
+def transform_wildfire_data(df):
     """
     Transforms Wildfire burned area data in a pandas DataFrame 
 
@@ -128,16 +125,15 @@ def transform_wildfire_data(file_path):
     5. Reorder the columns. 
     
     Args:
-        file_path (str): Path to the CSV file containing wildfire data.
+        df (str): Path to the CSV file containing wildfire data.
 
     Returns:
         pd.DataFrame: Transformed wildfire DataFrame.
     """
-    wildfire_df = load_and_clean_data(file_path)
     wildfire_columns_to_drop = ['gid_1']
-    wildfire_df = drop_irrelevant_columns(wildfire_df, wildfire_columns_to_drop)
+    df = drop_irrelevant_columns(df, wildfire_columns_to_drop).copy()
     columns_with_zeros = ["forest", "savannas", "shrublands_grasslands", "croplands", "other"]
-    wildfire_df = drop_rows_with_zeros(wildfire_df, columns_with_zeros)
+    df = drop_rows_with_zeros(df, columns_with_zeros).copy()
     wildfire_columns_rename = {'year': 'Year',
                                     'month':'Month',
                                     'gid_0':'Country_Code',
@@ -148,17 +144,16 @@ def transform_wildfire_data(file_path):
                                     'shrublands_grasslands':'Shrubs_Grasslands_BA',
                                     'croplands':'Croplands_BA',
                                     'other':'Other_BA'}
-    wildfire_df = rename_columns(wildfire_df, wildfire_columns_rename)
-    wildfire_df = create_quarterly_column(wildfire_df)
-    wildfire_df = wildfire_df.reindex(columns=['Year','Month','Quarter','Country_Code','Country_Name',
+    df = rename_columns(df, wildfire_columns_rename).copy()
+    df = create_quarterly_column(df).copy()
+    return df.reindex(columns=['Year','Month','Quarter','Country_Code','Country_Name',
                                                'Region_Name','Forest_BA','Savannas_BA', 
                                                'Shrubs_Grasslands_BA', 'Croplands_BA', 
                                                'Other_BA'])
-    return wildfire_df
-
-def transform_emissions_data(file_path):
+    
+def transform_emissions_data(df):
     """
-   Transforms Emissions data in a pandas DataFrame 
+    Transforms Emissions data in a pandas DataFrame 
 
     This function performs several operations on the DataFrame to prepare this data for analysis:
     1. Drop the uncessary columns.
@@ -168,27 +163,25 @@ def transform_emissions_data(file_path):
     5. Reorder the columns. 
     
     Args:
-        file_path (str): Path to the CSV file containing emissions data.
+        df (str): Path to the CSV file containing emissions data.
 
     Returns:
         pd.DataFrame: Transformed emissions DataFrame.
     """
-    emissions_df = load_and_clean_data(file_path)
     emissions_columns_to_drop = ['gid_1']
-    emissions_df = drop_irrelevant_columns(emissions_df, emissions_columns_to_drop)
+    df = drop_irrelevant_columns(df, emissions_columns_to_drop).copy()
     columns_with_zeros = ["CO2", "CO", "TPM", "PM25", "TPC", "NMHC", "OC", "CH4", "SO2", "BC", "NOx"]
-    emissions_df = drop_rows_with_zeros(emissions_df, columns_with_zeros)
+    df = drop_rows_with_zeros(df, columns_with_zeros).copy()
     emissions_columns_rename = {'year':'Year',
                                 'month':'Month',
                                 'gid_0':'Country_Code' ,
                                 'country':'Country_Name',
                                 'region':'Region_Name'}
-    emissions_df = rename_columns(emissions_df, emissions_columns_rename)
-    emissions_df = create_quarterly_column(emissions_df)
-    emissions_df = emissions_df.reindex(columns=['Year', 'Month', 'Quarter', 'Country_Code', 'Country_Name', 
+    df = rename_columns(df, emissions_columns_rename).copy()
+    df = create_quarterly_column(df).copy()
+    return df.reindex(columns=['Year', 'Month', 'Quarter', 'Country_Code', 'Country_Name', 
                                                  'Region_Name', 'CO2', 'CO', 'TPM', 'PM25', 'TPC', 'NMHC', 
                                                  'OC', 'CH4', 'SO2', 'BC', 'NOx'])
-    return emissions_df
 
 def main():
     """
@@ -199,50 +192,40 @@ def main():
         - Download and extract data.
         - Transform wildfire and emissions data.
         - Save the transformed data to a SQLite database.
-        - Fetch data from the database to verify and save to CSV files.
     
     Returns:
         None
     """
     wildfire_url = "https://effis-gwis-cms.s3.eu-west-1.amazonaws.com/apps/country.profile/MCD64A1_burned_area_full_dataset_2002-2023.zip"
     emissions_url = "https://effis-gwis-cms.s3.eu-west-1.amazonaws.com/apps/country.profile/emission_gfed_full_2002_2023.zip"
-   
-    #Directory for downloading and extracting data
-    download_path = "data"
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
-
-    download_and_extract(wildfire_url, download_path)
-    download_and_extract(emissions_url, download_path)
+    
+    wildfire_files = download_and_extract_in_memory(wildfire_url)
+    emissions_files = download_and_extract_in_memory(emissions_url)
     
     #File paths for the extracted CSV files
-    wildfire_file = os.path.join(download_path, 'MCD64A1_burned_area_full_dataset_2002-2023.csv')
-    emissions_file = os.path.join(download_path, 'emission_gfed_full_2002_2023.csv')
-   
+    wildfire_file_like = wildfire_files['MCD64A1_burned_area_full_dataset_2002-2023.csv']
+    emissions_file_like = emissions_files['emission_gfed_full_2002_2023.csv']
+    
+    wildfire_df = load_and_clean_data(wildfire_file_like)
+    emissions_df = load_and_clean_data(emissions_file_like)
+    
     #Transform the wildfire and emissions data
-    wildfire_df = transform_wildfire_data(wildfire_file)
-    emissions_df = transform_emissions_data(emissions_file)
-
+    wildfire_df = transform_wildfire_data(wildfire_df).copy()
+    emissions_df = transform_emissions_data(emissions_df).copy()
+    
     # Merge the wildfire and emission datasets to perform further analysis
-    merged_df = pd.merge(wildfire_df, emissions_df, how='inner')  
-    
-    #SQLite database path
-    db_path = os.path.join(download_path, 'wildfire_emissions_data.db')
-    engine = create_engine(f'sqlite:///{db_path}')
+    merged_df = pd.merge(wildfire_df, emissions_df, how='inner')
 
+    #SQLite database path
+    db_dir = 'data'
+    db_name = 'wildfire_burnedarea_emissions_data'
+    db_path = create_database(db_dir, db_name)
+    engine = create_engine(f'sqlite:///{db_path}')
+    
     #Save the processed data to the database
-    wildfire_df.to_sql('wildfire_data', engine, index=False, if_exists='replace')
-    emissions_df.to_sql('emissions_data', engine, index=False, if_exists='replace')
-    merged_df.to_sql('merged_table',engine,index=False, if_exists='replace')
-    
-    #Fetch data from the database to verify and save to CSV
-    fetched_wildfire_df = fetch_data_from_db(db_path, 'wildfire_data')
-    fetched_emissions_df = fetch_data_from_db(db_path, 'emissions_data')
-    fetched_merged_df = fetch_data_from_db(db_path, 'merged_table')
-    
-    fetched_wildfire_df.to_csv(os.path.join(download_path, 'cleaned_wildfire_burned_area_data.csv'), index=False)
-    fetched_emissions_df.to_csv(os.path.join(download_path, 'cleaned_emissions_data.csv'), index=False)
-    fetched_merged_df.to_csv(os.path.join(download_path, 'cleaned_merged_data.csv'), index=False)
+    wildfire_df.to_sql('wildfire_burnedarea_data', engine, index=False, if_exists='replace')
+    emissions_df.to_sql('wildfire_emissions_data', engine, index=False, if_exists='replace')
+    merged_df.to_sql('wildfire_merged_data', engine, index=False, if_exists='replace')
 
 if __name__ == "__main__":
     main()
