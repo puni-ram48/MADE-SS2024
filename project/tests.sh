7dfe43b1bcf8a906f27e481227db33539d960fca
Puneetha Dharmapura Shrirama <dspuneetha@gmail.com>
2024-06-11 18:57:23 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-echo ">>> Installing required packages..."
-pip install -r ./project/requirements.txt
-
-echo ">>> Running the tests ..."
+echo ">>> installing required packages..."
+pip install -r ./project/requirement.txt
+echo ">>> running the tests ..."
 python3 ./project/automated_testing.py
-
-# Check the exit status of the test run
-if [ $? -eq 0 ]; then
-    echo ">>> Tests ran successfully."
-else
-    echo ">>> Tests failed."
-fi
+echo ">>> tests ran sucessfully..."
