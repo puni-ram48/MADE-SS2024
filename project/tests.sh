24ace2dcd023d85724d3931670382b9a091171ba
Puneetha Dharmapura Shrirama <dspuneetha@gmail.com>
2024-06-11 19:17:42 +0200

    Delete project/tests.sh

--- project/tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo ">>> installing required packages..."
-pip install -r ./project/requirement.txt
-echo ">>> running the tests ..."
-python3 ./project/automated_testing.py
-echo ">>> tests ran sucessfully..."
