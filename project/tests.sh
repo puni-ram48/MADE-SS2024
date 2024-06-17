f46ac02c4cb3dc8f290c252f13a7e212f6531a97
Puneetha Dharmapura Shrirama <dspuneetha@gmail.com>
2024-06-17 16:49:45 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo ">>> installing required packages..."
-pip install -r ./requirements.txt
+pip install -r ./project/requirements.txt
 echo ">>> running the tests ..."
-if python3 ./automated_testing.py; then 
+if python3 ./project/automated_testing.py; then 
 echo ">>> tests ran sucessfully..."
 else 
 echo ">>> tests failed..."
