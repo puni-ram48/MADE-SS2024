3a04a8bd54a1c3b22ed0a6dd5d95137d80f3612b
Puneetha Dharmapura Shrirama <dspuneetha@gmail.com>
2024-06-21 09:23:09 +0200

    Update tests.sh
    added comment to change the path while running in local system

--- project/tests.sh
+++ project/tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#To execute the '.sh' file on your local system, ensure you modify the file paths accordingly to match your local directory structure.
+#pip install -r requirements.txt
+#if python3 automated_testing.py ;
 echo ">>> installing required packages..."
 pip install -r ./project/requirements.txt
 echo ">>> running the tests ..."
